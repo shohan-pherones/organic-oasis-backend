6e65d17897607fb4e431e36466f88c4697d1a6b2
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-11 16:03:05 +0600

    refactor: server

--- src/app.ts
+++ src/app.ts
@@ -1,57 +1,28 @@
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import express, { NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import helmet from "helmet";
 import { StatusCodes } from "http-status-codes";
 import morgan from "morgan";
-import AppError from "./app/errors/app.error";
+import globalErrorHandler from "./app/errors/global.error";
+import notFoundHandler from "./app/errors/not-found.error";
 import router from "./app/routes";
 
-const app = express();
+const app: Application = express();
 
-const configureMiddleware = () => {
-  app.use(helmet());
-  app.use(cors());
-  app.use(express.json());
-  app.use(cookieParser());
-  app.use(morgan("short"));
-};
+app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(morgan("short"));
+app.use(cookieParser());
 
-const initializeRoutes = () => {
-  app.use("/api/v1", router);
-  app.get("/api/v1/health", (req: Request, res: Response) => {
-    res.status(StatusCodes.OK).json({ message: "Server is running healthy!" });
-  });
-  app.use((req: Request, res: Response) => {
-    res.status(StatusCodes.NOT_FOUND).json({ message: "Route not found" });
-  });
-};
+app.use("/api/v1", router);
 
-const handleErrors = () => {
-  app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
+app.get("/api/v1/health", (req: Request, res: Response) => {
+  res.status(StatusCodes.OK).json({ message: "Server is running healthy!" });
+});
 
-    if (err instanceof AppError) {
-      res.status(err.statusCode).json({ message: err.message });
-    } else {
-      res
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: "Internal Server Error" });
-    }
-  });
+app.use(notFoundHandler);
+app.use(globalErrorHandler);
 
-  process.on("unhandledRejection", (reason: any, promise: Promise<any>) => {
-    console.error("Unhandled Rejection at:", promise, "reason:", reason);
-  });
-
-  process.on("uncaughtException", (error: Error) => {
-    console.error("Uncaught Exception:", error.message);
-  });
-};
-
-export const createApp = () => {
-  configureMiddleware();
-  initializeRoutes();
-  handleErrors();
-  return app;
-};
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,52 +1,39 @@
-import { Application } from "express";
-import http from "http";
-import { createApp } from "./app";
+import { Server } from "http";
+import app from "./app";
 import connectDB from "./app/config/db";
 import env from "./app/config/env";
 
-class Server {
-  private app: Application;
-  private PORT: number;
-  private serverInstance: http.Server | null = null;
+let server: Server;
 
-  constructor(port: number) {
-    this.app = createApp();
-    this.PORT = port;
-  }
+async function main() {
+  try {
+    await connectDB();
 
-  public static connectDatabaseOnce() {
-    connectDB();
-  }
-
-  public start() {
-    this.serverInstance = this.app.listen(this.PORT, () => {
-      console.log(`Server is running on http://localhost:${this.PORT}`);
+    server = app.listen(env.port, () => {
+      console.log(`🚀 Server is listening on port: ${env.port}`);
     });
-
-    process.on("SIGINT", this.stop.bind(this));
-    process.on("SIGTERM", this.stop.bind(this));
-  }
-
-  public stop() {
-    if (this.serverInstance) {
-      this.serverInstance.close(() => {
-        console.log(`Server on port ${this.PORT} stopped gracefully.`);
-        process.exit(0);
-      });
-    } else {
-      console.error("Server instance not found.");
-    }
+  } catch (error) {
+    console.error("❌ Error starting server:", error);
+    process.exit(1);
   }
 }
 
-Server.connectDatabaseOnce();
+main();
 
-const servers: Server[] = [];
-const numberOfInstances = 1;
+process.on("unhandledRejection", (reason) => {
+  console.error("👿 Unhandled Rejection detected, shutting down server...");
+  console.error(reason);
+  if (server) {
+    server.close(() => {
+      process.exit(1);
+    });
+  } else {
+    process.exit(1);
+  }
+});
 
-for (let i = 0; i < numberOfInstances; i++) {
-  const port = parseInt(env.port as string) + i;
-  const server = new Server(port);
-  servers.push(server);
-  server.start();
-}
+process.on("uncaughtException", (error) => {
+  console.error("👿 Uncaught Exception detected, shutting down server...");
+  console.error(error);
+  process.exit(1);
+});
