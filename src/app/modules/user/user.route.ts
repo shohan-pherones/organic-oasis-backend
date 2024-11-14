72055aa44b5a221299d96b9c45ca3b2b9b7b4a76
shohan-pherones <shohan.sub.56@gmail.com>
2024-11-14 23:50:52 +0600

    feature: category

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -7,6 +7,18 @@ import { UserValidations } from "./user.validation";
 
 const router: Router = express.Router();
 
+router.get("/", auth(USER_ROLE.admin), UserControllers.getAllUsers);
+router.get(
+  "/:userId",
+  auth(USER_ROLE.user, USER_ROLE.admin),
+  UserControllers.getAnUser
+);
+router.put(
+  "/:userId",
+  auth(USER_ROLE.user),
+  validate(UserValidations.updateUserSchema),
+  UserControllers.updateAnUser
+);
 router.post(
   "/register",
   validate(UserValidations.registerSchema),
@@ -21,18 +33,6 @@ router.post(
   "/refresh-token",
   validate(UserValidations.refreshTokenSchema),
   UserControllers.refreshToken
-);
-router.get("/all", auth(USER_ROLE.admin), UserControllers.getAllUsers);
-router.get(
-  "/:userId",
-  auth(USER_ROLE.user, USER_ROLE.admin),
-  UserControllers.getAnUser
-);
-router.put(
-  "/:userId",
-  auth(USER_ROLE.user),
-  validate(UserValidations.updateUserSchema),
-  UserControllers.updateAnUser
 );
 
 export default router;
